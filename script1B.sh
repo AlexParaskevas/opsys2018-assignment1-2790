df8f1a5acf4dcc73be5d2d8e99a3ada72d3ab979
Pelekan <32462531+Pelekan@users.noreply.github.com>
2018-12-09 23:59:04 +0200

    Add files via upload

--- script1B.sh
+++ script1B.sh
@@ -2,17 +2,11 @@
 
 urlcheck()
 {
-	pagetemp= echo "$1" |  cut -d '/' -f 3  #$1=$line
-	curl -s $line > /tmp/"$pagetemp".txt
+	pagetemp=`echo "$1" |  cut -d '/' -f 3`  #$1=$line
+	curl -s $1 > /tmp/"$pagetemp".txt
 }
 
 Afile=$1
-
-if [ ! -f $1 ]; then
-	echo "Wrong directory/cannot find List of Adresses"
-	exit $?
-fi
-
 
 dir_name=~/Desktop/opsysB
 
@@ -25,13 +19,13 @@ fi
 > ~/Desktop/opsysB/outfile.txt
 
 while IFS=$'\n' read line; do 
-    if [ ! $line == "#"* ]; then
+    if [[ ! $line == "#"* ]]; then
 	curlstatus=`curl -s -w "%{http_code}\n" "$line" -o /dev/null` #curl each webpage/line
 	pagetemp=`echo "$line" |  cut -d '/' -f 3` #keeps only the http://www.url.com/
 	if [ $curlstatus == "200" ]; then
 		if [ -f  ~/Desktop/opsysB/"$pagetemp".txt ]; then
 			curl -s $line > /tmp/"$pagetemp".txt 
-			if [ ! $(cmp -s ~/Desktop/opsysB/"$pagetemp".txt /tmp/"$pagetemp".txt) == "0" ]; then 
+			if [ $(cmp -s ~/Desktop/opsysB/"$pagetemp".txt /tmp/"$pagetemp".txt) != "0" ]; then 
 				echo $line >> ~/Desktop/opsysB/outfile.txt 
 				rm ~/Desktop/opsysB/"$pagetemp".txt  #removes the outdated output 
 				mv /tmp/"$pagetemp".txt ~/Desktop/opsysB  #replaces it with the updated	
@@ -39,7 +33,7 @@ while IFS=$'\n' read line; do
 		
 		else
 			echo "$line INIT" 			
-			urlcheck "$line" & #passes line as first argument
+			urlcheck "$line" & #passes line as first argument/with & runs in the backround
 		fi
 	else
 		echo "$line FAILED" 
