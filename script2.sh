e3e199bc0fbc9e5dc119e58d9e7f6b854c55f0e7
Pelekan <32462531+Pelekan@users.noreply.github.com>
2018-12-10 19:36:13 +0200

    Add files via upload

--- script2.sh
+++ script2.sh
@@ -3,11 +3,7 @@
 
 Afile=$1
 
-if [ -f $1 ]; then
-	echo "File $1 exists"
-
-else
-
+if [ ! -f $1 ]; then
 	echo "Wrong directory/cannot find compressed file"
 	exit $?
 fi
@@ -21,7 +17,7 @@ fi
 
 cd ~/Desktop/repos_temp
 
-tar -xvzf $1 
+tar -xvzf $1 #command tar extracts to the repos_temp directory
 
 if [ ! -d ~/Desktop/repos ]; then
 
@@ -29,50 +25,43 @@ if [ ! -d ~/Desktop/repos ]; then
 fi
 
 find ~/Desktop/repos_temp -type f -name "*.txt" | while read txt; do 
-
-	mv $txt ~/Desktop/repos
-
+	mv $txt ~/Desktop/repos #finds txt files in the temp dir and moves them to the repos dir
 done
 
-rm -rf ~/Desktop/repos_temp
-
-
-
 dir_name=~/Desktop/Assignments
-
 if [ ! -f $dir_name ]; then
 
 	mkdir -p $dir_name
 fi
 
-
 cd ~/Desktop/Assignments
 
-
-
 for file in ~/Desktop/repos/*.txt; do
+
 	while IFS=$'\n' read -r line; do
-		if [ ! $line == "#"* ]; then
-			if [ $line == "https"* ]; then
-		
+
+		if [[ ! $line == "#"* ]]; then
+			if [[ $line == "https"* ]]; then
+			
 				repourl=`echo "$line" | cut -d'/' -f 5` #keeps the fixed repo link
 				if [ ! -d ~/Desktop/Assignments/$repourl ]; then
-
+				
 					mkdir ~/Desktop/Assignments/$repourl
-
 				fi
-		
+	
 				git clone $line ~/Desktop/Assignments/$repourl
-				if [$? == "0"]; then #if cloning is successfull then returns 0
+				if [ $? == "0" ]; then #if cloning is successfull then returns 0
 			
 					echo "$line Cloning OK";
 				fi
+
 			else
 				echo "$line Cloning FAILED"
 	
 			fi
 
-	done < "$file"
+	done < "$1"
+
 done 
 
 
@@ -81,7 +70,7 @@ done
 
 for file1 in ~/Desktop/Assignments/*; do
 
-	countdir=`find $file1/* -maxdepth 0 -type d | wc -l`
+	countdir=`find $file1/* -type d | wc -l`
 	counttxt=`find $file1/* -type f -name '*.txt' | wc -l`
 	countother=`find $file1/* | wc -l` #checks all the files cause of *
 	countother=$(($countother-$counttxt-$countdir)) #remaining other files except dirs and txts
@@ -104,17 +93,3 @@ for file1 in ~/Desktop/Assignments/*; do
 
 
 done			
-
-	
-	
-
-
-		
-
-
-
-
-
-
-
-
