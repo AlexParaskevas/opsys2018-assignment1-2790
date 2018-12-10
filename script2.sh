ba00849fa48b877e01b80aa341db70d5d0160a14
Pelekan <32462531+Pelekan@users.noreply.github.com>
2018-12-10 19:30:46 +0200

    Add files via upload

--- script2.sh
+++ script2.sh
@@ -3,7 +3,11 @@
 
 Afile=$1
 
-if [ ! -f $1 ]; then
+if [ -f $1 ]; then
+	echo "File $1 exists"
+
+else
+
 	echo "Wrong directory/cannot find compressed file"
 	exit $?
 fi
@@ -17,7 +21,7 @@ fi
 
 cd ~/Desktop/repos_temp
 
-tar -xvzf $1 #command tar extracts to the repos_temp directory
+tar -xvzf $1 
 
 if [ ! -d ~/Desktop/repos ]; then
 
@@ -25,28 +29,39 @@ if [ ! -d ~/Desktop/repos ]; then
 fi
 
 find ~/Desktop/repos_temp -type f -name "*.txt" | while read txt; do 
-	mv $txt ~/Desktop/repos #finds txt files in the temp dir and moves them to the repos dir
+
+	mv $txt ~/Desktop/repos
+
 done
 
+rm -rf ~/Desktop/repos_temp
+
+
+
 dir_name=~/Desktop/Assignments
+
 if [ ! -f $dir_name ]; then
 
 	mkdir -p $dir_name
 fi
 
+
 cd ~/Desktop/Assignments
+
+
 
 for file in ~/Desktop/repos/*.txt; do
 	while IFS=$'\n' read -r line; do
-		if [[ ! $line == "#"* ]]; then
+		if [ ! $line == "#"* ]; then
 			if [ $line == "https"* ]; then
-			
+		
 				repourl=`echo "$line" | cut -d'/' -f 5` #keeps the fixed repo link
 				if [ ! -d ~/Desktop/Assignments/$repourl ]; then
-				
+
 					mkdir ~/Desktop/Assignments/$repourl
+
 				fi
-	
+		
 				git clone $line ~/Desktop/Assignments/$repourl
 				if [$? == "0"]; then #if cloning is successfull then returns 0
 			
@@ -57,7 +72,7 @@ for file in ~/Desktop/repos/*.txt; do
 	
 			fi
 
-	done < "$1"
+	done < "$file"
 done 
 
 
@@ -66,7 +81,7 @@ done
 
 for file1 in ~/Desktop/Assignments/*; do
 
-	countdir=`find $file1/* -type d | wc -l`
+	countdir=`find $file1/* -maxdepth 0 -type d | wc -l`
 	counttxt=`find $file1/* -type f -name '*.txt' | wc -l`
 	countother=`find $file1/* | wc -l` #checks all the files cause of *
 	countother=$(($countother-$counttxt-$countdir)) #remaining other files except dirs and txts
@@ -89,3 +104,17 @@ for file1 in ~/Desktop/Assignments/*; do
 
 
 done			
+
+	
+	
+
+
+		
+
+
+
+
+
+
+
+
